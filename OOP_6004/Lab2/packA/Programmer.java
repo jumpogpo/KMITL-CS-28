0f2ff1ddd9000e5606c21ae94095dd3a58e55564
Viper <chokhun191@gmail.com>
2023-01-17 13:58:21 +0700

    Update OOP Lab2

---
+++ OOP_6004/Lab2/packA/Programmer.java
@@ -0,0 +1,51 @@
+package packA;
+
+public class Programmer {
+    private String name;
+    protected int salary;
+    private int experience;
+
+    public Programmer(String n, int exp, int sal) {
+        name = n;
+        experience = exp;
+        salary = sal;
+    }
+
+    public Programmer(String n) {
+        name = n;
+    }
+
+    public Programmer() {}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSalary(int newSalaray) {
+        salary = newSalaray;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setExperience(int exp) {
+        experience = exp;
+    }
+
+    public int getExeperience() {
+        return experience;
+    }
+
+    public String toString() {
+        return "Programmer [name=" + name + ", salary=" + salary + ", experience=" + experience + "]";
+    }
+
+    public void sayHi() {
+        System.out.println("hi from" + name);
+    }
+}
