81ba29e9d3c9162c34e4d999170be6dc5b496dc3
Viper <chokhun191@gmail.com>
2023-02-21 11:03:40 +0700

    Update OOP

---
+++ OOP_6004/Mini-Project/Main.java
@@ -0,0 +1,7 @@
+import src.App;
+
+public class Main {
+    public static void main(String[] args) {
+        new App();
+    }
+}
---
+++ OOP_6004/Mini-Project/src/App.java
@@ -0,0 +1,190 @@
+package src;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.GridLayout;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class App {
+    private Random rand = new Random();
+    private JFrame f;
+    private JPanel top, middle;
+    private JButton btnNew, currentOpenButton;
+    private JLabel lbLevel;
+    private Icon questionMarkIcon;
+    private int tableSize = 4, alreadyOpen = 0, level = 1, count = 0, currentRandNumber = rand.nextInt(100);
+    private boolean cooldown = false;
+    AllButtonListener buttonAction = new AllButtonListener();
+
+    public App(){
+        f = new JFrame("Match Numbers");
+        f.setSize(500,450);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setResizable(false);
+        f.setLayout(new BorderLayout());
+        detail();
+        f.setVisible(true);
+    }
+
+    private void detail(){
+        try {
+            Image resultImageQuestionMarkImage = new ImageIcon("src/../picture/QuestionMark.png").getImage().getScaledInstance(150, 80, Image.SCALE_DEFAULT);
+            questionMarkIcon = (Icon) new ImageIcon(resultImageQuestionMarkImage);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        top = new JPanel();
+        middle = new JPanel();
+        btnNew = new JButton("New Game");
+        lbLevel = new JLabel("Level: " + level);
+
+        btnNew.addActionListener(buttonAction);
+
+        newTable();
+
+        top.setBackground(new java.awt.Color(33, 33, 33));
+        middle.setBackground(new java.awt.Color(33, 33, 33));
+
+        btnNew.setPreferredSize(new Dimension(150,50));
+        lbLevel.setPreferredSize(new Dimension(65,50));
+
+        middle.setLayout(new GridLayout(tableSize / 2, tableSize / 2));
+
+        lbLevel.setForeground(Color.WHITE);
+
+        top.add(btnNew);
+        top.add(lbLevel);
+
+        f.add(top, BorderLayout.NORTH);
+        f.add(middle, BorderLayout.CENTER);
+    }
+
+    private void newTable() {
+        ArrayList<JButton> gameButton = new ArrayList<>();
+        middle.removeAll(); 
+
+        for (int i = 1; i < (tableSize + 1); i++) {
+            JButton button = new JButton(questionMarkIcon);
+            button.setForeground(Color.WHITE);
+            button.setFont(new Font("Times New Roman", Font.PLAIN, 30));
+            button.setPreferredSize(new Dimension(150,50));
+            button.addActionListener(buttonAction);
+            button.setText(null);
+            middle.add(button);
+            gameButton.add(button);
+        }
+
+        ArrayList<JButton> clonedGameButton = new ArrayList<>(gameButton.size());
+
+        for (JButton button : gameButton) {
+            clonedGameButton.add(button);
+        }
+
+        currentOpenButton = null;
+        cooldown = false;
+        alreadyOpen = 0;
+        for (JButton button : clonedGameButton) {
+            button.setForeground(Color.WHITE);
+            button.setIcon(questionMarkIcon);
+        }
+
+        for (int a = 0; a < tableSize; a++) {
+            if (count == 2) {
+                count = 0;
+                currentRandNumber = rand.nextInt(100);
+            }
+
+            int index = (int) (Math.random() * clonedGameButton.size());
+            clonedGameButton.get(index).setText(String.valueOf(currentRandNumber));
+            clonedGameButton.remove(index);
+            count++;
+        }
+    }
+
+    private class AllButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent ev) {
+            JButton source = (JButton) ev.getSource();
+
+            if (source == btnNew) {
+                level = 0;
+                lbLevel.setText("Level: " + level);
+                newTable();
+            } else {
+                if (currentOpenButton == null) {
+                    if (source.getIcon() == questionMarkIcon && !cooldown) {
+                        currentOpenButton = source;
+                        source.setForeground(Color.BLACK);
+                        source.setIcon(null);
+                    }
+                } else if (currentOpenButton.getText().equals(source.getText()) && source.getIcon() == questionMarkIcon && !cooldown) {
+                    currentOpenButton = null;
+                    source.setForeground(Color.BLACK);
+                    source.setIcon(null);
+                    alreadyOpen += 2;
+
+                    if (alreadyOpen == tableSize) {
+                        cooldown = true;
+                        alreadyOpen = 0;
+                        level++;
+
+                        if (level % 5 == 0) {
+                            tableSize += 2;
+                        }
+
+                        Timer timer = new Timer(2000, new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                lbLevel.setText("Level: " + level);
+                                newTable();
+                            }
+                        });
+
+                        timer.setRepeats(false);
+                        timer.start();
+                    }
+                
+                } else if (!currentOpenButton.getText().equals(source.getText()) && source.getIcon() == questionMarkIcon && !cooldown) {
+                    cooldown = true;
+                    source.setForeground(Color.BLACK);
+                    source.setIcon(null);
+
+                    Timer timer = new Timer(2000, new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            source.setForeground(Color.WHITE);
+                            source.setIcon(questionMarkIcon);
+
+                            if (currentOpenButton != null) {
+                                currentOpenButton.setForeground(Color.WHITE);
+                                currentOpenButton.setIcon(questionMarkIcon);
+                                currentOpenButton = null;
+                            }
+                            
+                            cooldown = false;
+                        }
+                    });
+
+                    timer.setRepeats(false);
+                    timer.start();
+                }
+            }
+        }
+    }
+}
