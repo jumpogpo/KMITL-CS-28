a52a5bce343ba5cd123951ab6549de9870f8dc03
Viper <chokhun191@gmail.com>
2023-12-04 22:43:39 +0700

    Update code

--- Year2/Analysis_and_Desgin_of_Algorithms_6236/Lab1/Main.java
+++ Year2/Analysis_and_Desgin_of_Algorithms_6236/Lab1/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
+import static java.lang.Math.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double a, b, c;
-        double root1, root2;
 
         System.out.println("Formula:\nAx^2 + bx + c");
         System.out.print("Please input a: ");
@@ -22,21 +22,19 @@ public class Main {
         c = sc.nextDouble();
         sc.close();
 
-        double determinant = b * b - 4 * a * c;
-
-        if (determinant > 0) {
-            root1 = (-b + Math.sqrt(determinant)) / (2 * a);
-            root2 = (-b - Math.sqrt(determinant)) / (2 * a);
-      
-            System.out.format("root1 = %.2f and root2 = %.2f%n", root1, root2);
-        } else if (determinant == 0) {
-            root1 = root2 = -b / (2 * a);
-            System.out.format("root1 = root2 = %.2f%n", root1);
-        } else {      
-            double real = -b / (2 * a);
-            double imaginary = Math.sqrt(-determinant) / (2 * a);
-            System.out.format("root1 = %.2f+%.2fi", real, imaginary);
-            System.out.format("\nroot2 = %.2f-%.2fi%n", real, imaginary);
+        double d = b * b - 4 * a * c;
+        double sqrt_val = sqrt(abs(d));
+ 
+        if (d > 0) {
+            System.out.println("\nRoots are real and different");
+ 
+            System.out.println((double)(-b + sqrt_val) / (2 * a) + "\n" + (double)(-b - sqrt_val) / (2 * a));
+        } else if (d == 0) {
+            System.out.println("\nRoots are real and same");
+            System.out.println(-(double)b / (2 * a) + "\n" + -(double)b / (2 * a));
+        } else {
+            System.out.println("\nRoots are complex");
+            System.out.println(-(double)b / (2 * a) + " + i" + sqrt_val / (2 * a) + "\n" + -(double)b / (2 * a) + " - i" + sqrt_val / (2 * a));
         }
     }
 }
