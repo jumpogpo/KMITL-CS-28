f12cf0212d63442a1e2185f7ce0705bd8586850e
Viper <chokhun191@gmail.com>
2024-02-19 16:02:37 +0700

    ✨ (init): Add Lab10
    Add Lab10

---
+++ Year2/Computer_Graphics_6014/Lab10/Libs/Shader.h
@@ -0,0 +1,36 @@
+#ifndef SHADER____H
+#define SHADER____H
+
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <string>
+#include <string.h>
+
+#include <GL/glew.h>
+
+class Shader
+{
+    public:
+        Shader();
+        ~Shader();
+
+        void CreateFromString (const char* vertexCode, const char* fragmentCode);
+        void CreateFromFiles (const char* vertexLocation, const char* fragmentLocation);
+        std::string ReadFile(const char* fileLocation);
+
+        void UseShader();
+        void ClearShader();
+
+        GLuint GetUniformLocation(const char* uniformName) {return glGetUniformLocation(shader, uniformName);}
+        GLuint GetModelLocation() { return glGetUniformLocation(shader, "model"); }
+        GLuint GetProjectionLocation() { return glGetUniformLocation(shader, "projection"); }
+        GLuint GetViewLocation() { return glGetUniformLocation(shader, "view"); }
+    private:
+        GLuint shader;
+        void CompileShaders(const char* vertexCode, const char* fragmentCode);
+        void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType);
+
+};
+
+#endif
